21083f78d697a65df1cb344413a28d3abe6adf45
Wellington Lima <wellington.map@hotmail.com>
2024-07-15 15:01:32 -0300

    feat: add modify user method

---
+++ internal/users/modify.go
@@ -0,0 +1,50 @@
+package users
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi"
+)
+
+func (h *handler) Modify(rw http.ResponseWriter, r *http.Request) {
+	u := new(User)
+
+	err := json.NewDecoder(r.Body).Decode(u)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if u.Name == "" {
+		http.Error(rw, ErrNameRequired.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = Update(h.db, int64(id), u)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rw.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(u)
+}
+
+func Update(db *sql.DB, id int64, u *User) error {
+	u.ModifiedAt = time.Now()
+
+	stmt := `update "users" set "name"=$1, "modified_at"=$2 where id=$3`
+	_, err := db.Exec(stmt, u.Name, u.ModifiedAt, id)
+
+	return err
+}
