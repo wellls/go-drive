d95bf5861f286540588cca7d03b22c273ae13a9e
Wellington Lima <wellington.map@hotmail.com>
2024-07-16 09:07:11 -0300

    feat: create folder struct with new and validate methods

---
+++ internal/folders/entity.go
@@ -0,0 +1,41 @@
+package folders
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrNameRequired = errors.New("name is required")
+)
+
+func New(name string, parentID int64) (*Folder, error) {
+	f := Folder{
+		Name:     name,
+		ParentID: parentID,
+	}
+
+	err := f.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return &f, nil
+}
+
+type Folder struct {
+	ID         int64     `json:"id"`
+	ParentID   int64     `json:"parent_id"`
+	Name       string    `json:"name"`
+	CreatedAt  time.Time `json:"created_at"`
+	ModifiedAt time.Time `json:"modified_at"`
+	Deleted    bool      `json:"-"`
+}
+
+func (f *Folder) Validate() error {
+	if f.Name == "" {
+		return ErrNameRequired
+	}
+
+	return nil
+}
