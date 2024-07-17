a60efd7458fc1ede478127c42b84df3f323bed23
Wellington Lima <wellington.map@hotmail.com>
2024-07-17 09:48:14 -0300

    feat: create List Files method

---
+++ internal/files/list.go
@@ -0,0 +1,25 @@
+package files
+
+import "database/sql"
+
+func List(db *sql.DB, folderID int64) ([]File, error) {
+	stmt := `select * from "files" where folder_id = $1`
+	rows, err := db.Query(stmt, folderID)
+	if err != nil {
+		return nil, err
+	}
+
+	files := make([]File, 0)
+	for rows.Next() {
+		var f File
+
+		err := rows.Scan(&f.ID, &f.FolderID, &f.OwnerID, &f.Name, &f.Type, &f.Path, &f.CreatedAt, &f.ModifiedAt, &f.Deleted)
+		if err != nil {
+			continue
+		}
+
+		files = append(files, f)
+	}
+
+	return files, nil
+}
